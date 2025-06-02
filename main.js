dea4ecf178795544add6e29373a1642e74154333
gayv4468 <gayv4468@gmail.com>
2025-06-02 12:48:38 +0000

    adiçao do evento de click na div.custom-checkbox

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function showItemsList() {
          <div class="item">
                  <div>
                   <input type="checkbox" name="list" id="item-${index}" ${item.checked === true ? "checked" : ""}>
-                  <div class="custom-checkbox">
+                 <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                     <img src="./Assets/checked.svg" alt="checked">
                   </div>
                      <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
